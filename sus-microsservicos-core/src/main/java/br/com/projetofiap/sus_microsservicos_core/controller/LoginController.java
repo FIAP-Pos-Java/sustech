bc4262e1c056ada06f4c0c4f731c4d4d54d02a4a
elias pinheiro pereira <eliaspinheiropereiraa@gmail.com>
2026-02-03 21:23:00 -0300

    finalizando o v1.0 da criação de Login

--- sus-microsservicos-core/src/main/java/br/com/projetofiap/sus_microsservicos_core/controller/LoginController.java
+++ sus-microsservicos-core/src/main/java/br/com/projetofiap/sus_microsservicos_core/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class LoginController {
     private final Logger logger = LoggerFactory.getLogger(LoginController.class);
     private final LoginService loginService;
 
+    @GetMapping
     public ResponseEntity<Page<BuscarLoginDTO>> buscarTodosLogins(
             @RequestParam(value = "page") int page,
             @RequestParam(value = "size") int size
